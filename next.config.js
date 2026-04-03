7fad6c7dc438e811fdf756a0fb1d04be3fb9d74d
spidey889 <vinitrajpurohit09@gmail.com>
2026-04-03 19:56:13 +0530

    fix(pwa): use stable push worker path

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
+  customWorkerDir: '__no_custom_worker__',
+  importScripts: ['/push-worker.js'],
   runtimeCaching: [],
   buildExcludes: [/./],
 })
